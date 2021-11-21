3b35f8c7df2ad362f628ad0a8543f393192be309
dosora1 <dosora9@gmail.com>
2021-11-22 02:36:41 +0900

    Create CU_1912.cpp

---
+++ CU_1912.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int fac(int n) {
+	if (n == 1) return 1;
+	else return fac(n - 1) * n;
+}
+
+int main() {
+	int n;
+	cin >> n;
+		
+	cout << fac(n);
+	return 0;
+}
